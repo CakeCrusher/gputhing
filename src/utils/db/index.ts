aeee25af55d9ed0f73e2c0e4732c185d886e5cd1
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2023-01-14 15:37:26 -0500

    firebase service account json migrate to .env

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,5 +1,18 @@
 import admin, { ServiceAccount } from 'firebase-admin';
-import serviceAccount from '../../../serviceAccountKey.json';
+
+const serviceAccount = {
+  "type": process.env.SA_TYPE,
+  "project_id": process.env.SA_PROJECT_ID,
+  "private_key_id": process.env.SA_PRIVATE_KEY_ID,
+  "private_key": process.env.SA_PRIVATE_KEY,
+  "client_email": process.env.SA_CLIENT_EMAIL,
+  "client_id": process.env.SA_CLIENT_ID,
+  "auth_uri": process.env.SA_AUTH_URI,
+  "token_uri": process.env.SA_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.SA_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.SA_CLIENT_X509_CERT_URL
+}
+
 
 if (!admin.apps.length) {
   try {
